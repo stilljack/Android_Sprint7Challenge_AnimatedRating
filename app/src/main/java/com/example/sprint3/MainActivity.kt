4d5e271eea2472ac49477e7ba149264d8ab2a5f5
stilljack <stilljack@gmail.comconfig --global user.email stilljack@gmail.com>
2019-08-23 08:16:27 -0700

    jack seymour, "Android_Sprint7Challenge_AnimatedRating" AND3/sprint 3

---
+++ app/src/main/java/com/example/sprint3/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.sprint3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+//.0.planning...
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
